7f79896ee9a7ab6f33d66dbd7c45a0527ee17bbd
Alanna Zhou <alanna.zhou@gmail.com>
2018-04-11 20:25:07 -0700

    Finish a functional but not good problem5

--- problem5.cpp
+++ problem5.cpp
@@ -14,10 +14,38 @@
 
 using namespace std;
 
+bool IsDivisible(int multiple, int maximum) {
+    for (int i = 1; i <= maximum; i++) {
+        if (multiple % i != 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+long long GetSmallestMultiple(int maximum) {
+    long long multiple = 1;
+    for (int i = 1; i <= maximum; i++) {
+        if (multiple % i != 0) {
+            multiple *= i;
+        }
+    }
+    long long smallest_multiple = multiple;
+    for (int i = multiple; i >= 1; i--) {
+        if (IsDivisible(i, maximum) && smallest_multiple > i) {
+            smallest_multiple = i;
+        }
+    }
+
+    return smallest_multiple;
+}
+
 
 
 int main() {
-    cout << "Hello World" << endl;
+    cout << "Smallest Multiple: " << GetSmallestMultiple(20) << endl;
 
     return 0;
 }
