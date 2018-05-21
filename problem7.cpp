4ab44e3158f1c8404d8035b5bb71c824e03e331f
Alanna Zhou <alanna.zhou@gmail.com>
2018-05-21 12:13:27 -0700

    Begin problem7

--- problem7.cpp
+++ problem7.cpp
@@ -4,13 +4,48 @@
  * 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
  * What is the 10 001st prime number?
  *
+ * notes: all even numbers above 2 are not prime
+ *
+ * 0 is false
+ *
  */
 
 #include <iostream>;
 
 using namespace std;
 
+bool isPrime(int num) {
+    if (num < 2) {
+        return false;
+    }
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int nthPrime(int n) {
+//    if (n == 1) {
+//        return 2;
+//    }
+//    int i = 2;
+//    while (isPrime(i) == false) {
+//        i++;
+//        return i;
+//    }
+//
+
+    return 0;
+}
+
+
 int main() {
-    cout << "hello" << endl;
+    cout << "isPrime(1): " << isPrime(1) << endl;
+    cout << "isPrime(5): " << isPrime(5) << endl;
+    cout << "isPrime(4): " << isPrime(4) << endl;
+    cout << "isPrime(15): " << isPrime(15) << endl;
+
     return 0;
 }
