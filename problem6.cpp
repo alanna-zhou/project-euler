fa0608122cb3cb9f00af674d27d3e3d89b228257
Alanna Zhou <alanna.zhou@gmail.com>
2018-05-21 11:55:11 -0700

    Finish problem6

--- problem6.cpp
+++ problem6.cpp
@@ -2,9 +2,9 @@
  * Problem 6: Sum Square Difference
  *
  * The sum of the squares of the first ten natural numbers is,
- * 12 + 22 + ... + 102 = 385
+ * 1^2 + 2^2 + ... + 10^2 = 385
  * The square of the sum of the first ten natural numbers is,
- * (1 + 2 + ... + 10)2 = 552 =
+ * (1 + 2 + ... + 10)^2 = 55^2 = 3025
  * Hence the difference between the sum of the squares of the first ten natural numbers
  * and the square of the sum is 3025 − 385 = 2640.
  *
@@ -18,9 +18,31 @@
 
 using namespace std;
 
+int sumOfSquares() {
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+        sum += i*i;
+    }
+    return sum;
+}
+
+int squareOfSum() {
+    int square = 0;
+    for (int i = 1; i <= 100; i++) {
+        square += i;
+    }
+    square *= square;
+    return square;
+}
+
+int sumSquareDiff(int sum, int square) {
+    return square - sum;
+}
 
 int main() {
-    cout << "Hello" << endl;
+//    cout << sumOfSquares() << endl;
+//    cout << squareOfSum() << endl;
+    cout << sumSquareDiff(sumOfSquares(), squareOfSum()) << endl;
     return 0;
 }
 
