322b0d7cd231b27bfa1bbd0d135ed9b093ea09a4
alanna.zhou@gmail.com <alanna.zhou@gmail.com>
2018-07-20 14:47:19 -0700

    Move number to text file and finish example solution

--- cmake-build-debug/CMakeFiles/project_euler.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/project_euler.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/alanna/Documents/Code/project-euler/test.cpp" "/Users/alanna/Documents/Code/project-euler/cmake-build-debug/CMakeFiles/project_euler.dir/test.cpp.o"
+  "/Users/alanna/Documents/Code/project-euler/problem8.cpp" "/Users/alanna/Documents/Code/project-euler/cmake-build-debug/CMakeFiles/project_euler.dir/problem8.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
